1f7a7f6d1c3fae9678fde831638ff4db9e4df0fa
Mario Wagner <ma_wa@gmx.at>
2020-02-10 13:36:21 +0100

    Created the Chiffre Algorithm

--- src/com/codersbay/caesarchiffre/Aufgabe5_3_CaesarChiffre.java
+++ src/com/codersbay/caesarchiffre/Aufgabe5_3_CaesarChiffre.java
@@ -1,7 +1,23 @@
 package com.codersbay.caesarchiffre;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Aufgabe5_3_CaesarChiffre {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String text = scan.nextLine();
+        System.out.println(text);
 
+        Random randomNumber = new Random();
+        int key = randomNumber.nextInt(27);
+        System.out.println("key: " + key);
+
+        String chiffre = "";
+        for (int i = 0; i < text.length(); i++) {
+            char c = (char) (text.charAt(i) + key);
+            chiffre += c;
+        }
+        System.out.println(chiffre);
     }
 }
